eaee588769ff6340083b035413b2080aafacc06c
electrp <william.bender@digipen.edu>
2026-01-07 22:15:15 -0800

    Update scripts for visual studio generation

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,2 @@
 call setup.bat
-docker run --rm -v "../":/work -it --entrypoint bash factory-ds:latest ./scripts/build-toolchained.sh
+docker run --rm -v "../":/work -t --entrypoint bash factory-ds:latest ./scripts/build-toolchained.sh
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 ./setup.sh
-docker run --rm -v "../":/work -it --entrypoint bash factory-ds:latest ./scripts/build-toolchained.sh
+docker run --rm -v "../":/work -t --entrypoint bash factory-ds:latest ./scripts/build-toolchained.sh
