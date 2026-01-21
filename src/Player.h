6fa5a19c44da7b2a332bc0951f4bf16925878db7
sangbeom.kim <231103374+rdsangbeomkim@users.noreply.github.com>
2026-01-20 18:08:23 -0800

    Init player

---
+++ src/Player.h
@@ -0,0 +1,21 @@
+﻿// Sangbeom Kim
+// 01/20/2026
+#pragma once
+
+typedef float fixed;
+
+class Player 
+{
+	Player(fixed position[]) : position(position) {}
+
+	enum State {
+		MOVING, MINING, BUILDING, NUM_STATE
+	};
+
+	void Update();
+
+private:
+	fixed* position = new fixed[3];
+
+	State state;
+};
