c8a2064fcbdcc717dd54c6c56dbafafbb2b5b8a6
electrp <william.bender@digipen.edu>
2026-01-22 12:18:41 -0800

    Add some conversions between transform types

--- src/Transform.cpp
+++ src/Transform.cpp
@@ -3,3 +3,77 @@
 //
 
 #include "Transform.h"
+
+GridTransform::GridTransform(int32 x, int32 y): x(x),
+                                                y(y)
+{
+}
+
+GridTransform::GridTransform(): x(0), y(0)
+{}
+
+GridTransform::GridTransform(const GridTransform& other): x(other.x),
+                                                          y(other.y)
+{
+}
+
+GridTransform::GridTransform(GridTransform&& other) noexcept: x(other.x),
+                                                              y(other.y)
+{
+}
+
+GridTransform& GridTransform::operator=(const GridTransform& other)
+{
+    x = other.x;
+    y = other.y;
+    return *this;
+}
+
+GridTransform& GridTransform::operator=(GridTransform&& other) noexcept
+{
+    x = other.x;
+    y = other.y;
+    return *this;
+}
+
+GridTransform::GridTransform(Transform const& transform)
+{
+    x = transform.pos.X().GetInt();
+    y = transform.pos.Y().GetInt();
+}
+
+Transform::Transform(Vec2 pos, u8 layer) : pos(pos), layer(layer)
+{
+}
+
+Transform::Transform() : pos({}), layer(0)
+{
+}
+
+Transform::Transform(const Transform& other) : pos(other.pos), layer(other.layer)
+{
+}
+
+Transform::Transform(Transform&& other) noexcept : pos(other.pos), layer(other.layer)
+{
+}
+
+Transform& Transform::operator=(const Transform& other)
+{
+    pos = other.pos;
+    layer = other.layer;
+    return *this;
+}
+
+Transform& Transform::operator=(Transform&& other) noexcept
+{
+    pos = other.pos;
+    layer = other.layer;
+    return *this;
+}
+
+Transform::Transform(GridTransform const& transform)
+{
+    pos.X() = transform.x;
+    pos.Y() = transform.y;
+}
