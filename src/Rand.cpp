cf2871081bf38cf0b49b8e9879dbb44842cb2961
electrp <william.bender@digipen.edu>
2026-01-22 14:51:07 -0800

    RNG

---
+++ src/Rand.cpp
@@ -0,0 +1,42 @@
+﻿//
+// Created by willb on 1/22/2026.
+//
+
+#include "Rand.h"
+
+#include <chrono>
+
+u32 rand::GenerateNextU32_1(u32 seed)
+{
+    return GenerateNextLCG<u32, 2891336453, 1>(seed);
+}
+
+u32 rand::GenerateNextU32_2(u32 seed)
+{
+    return GenerateNextLCG<u32, 29943829, 1>(seed);
+}
+
+u32 rand::GenerateNextU32_3(u32 seed)
+{
+    return GenerateNextLCG<u32, 1664525, 1013904223>(seed);
+}
+
+u32 rand::GenerateNextU32_4(u32 seed)
+{
+    return GenerateNextLCG<u32, 134775813, 1>(seed);
+}
+
+// Defualt seed, good enough
+u32 rand_seed =
+    std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+
+u32 rand::Rand()
+{
+    rand_seed = GenerateNextU32_1(rand_seed);
+    return rand_seed;
+}
+
+void rand::SRand(u32 seed)
+{
+    rand_seed = seed;
+}
---
+++ src/Rand.h
@@ -0,0 +1,25 @@
+﻿//
+// Created by willb on 1/22/2026.
+//
+
+#pragma once
+
+#include <nds/ndstypes.h>
+
+namespace rand {
+    //https://en.wikipedia.org/wiki/Linear_congruential_generator
+    template<typename INT, INT A, INT C>
+    INT GenerateNextLCG(INT seed)
+    {
+        return seed * A + C;
+    }
+
+    // Different LCG functions with different seeds (uses GenerateNext)
+    u32 GenerateNextU32_1(u32 seed);
+    u32 GenerateNextU32_2(u32 seed);
+    u32 GenerateNextU32_3(u32 seed);
+    u32 GenerateNextU32_4(u32 seed);
+
+    u32 Rand();
+    void SRand(u32 seed);
+}
