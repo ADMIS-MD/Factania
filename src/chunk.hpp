d85cabf4dcc2f2d45032c34bfb06b3ce6beffa3f
electrp <william.bender@digipen.edu>
2026-01-22 12:26:32 -0800

    Add default for chunk

--- src/chunk.hpp
+++ src/chunk.hpp
@@ -17,8 +17,9 @@ struct Sprite {
 };
 
 // Chunks in their most beefy state
-struct Chunk {
-    Sprite cached_sprites[64];
+struct Chunk
+{
+	Sprite cached_sprites[64] {0};
     entt::entity top_entity_ids[64]; // The topmost object's entity id, if it has an entity on it
     entt::entity surrounding_chunks[8];
 };
